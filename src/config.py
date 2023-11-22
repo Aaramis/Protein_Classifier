1d62b471fffab6f76bf0b2d0734d80651a222f40
Aaramis <auguste.gardette@hotmail.fr>
2023-11-22 15:32:25 +0100

    Initialized parser

--- src/config.py
+++ src/config.py
@@ -1,79 +1,113 @@
-# config.py
-
+from typing import Optional
 import argparse
 import os
 import torch
 from log import write_logs, LogStatus
 
-def parse_args():
-    
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Protein Classifier Configuration')
 
     # Command line arguments for directory
     main_args = parser.add_argument_group('Directory arguments')
     main_args.add_argument('--data_path',
-                        type=str,
-                        default=os.path.abspath('./random_split'),
-                        help='Path to the data directory')
+                           type=str,
+                           default=os.path.abspath('./random_split'),
+                           help='Path to the data directory')
     main_args.add_argument('--log_path',
-                        type=str,
-                        default=os.path.abspath('./log'),
-                        help='Path to the log directory')
+                           type=str,
+                           default=os.path.abspath('./log'),
+                           help='Path to the log directory')
     main_args.add_argument('--output_path', "-o",
-                        type=str,
-                        default=os.path.abspath('./output'),
-                       help='Path to the output directory')   
+                           type=str,
+                           default=os.path.abspath('./output'),
+                           help='Path to the output directory')
 
     # Command line arguments for hyperparameters
     model_args = parser.add_argument_group('Models hyperparameters arguments')
 
-    model_args.add_argument('--seq_max_len'
-                            , type=int, default=120
-                            , help='Maximum length of sequences')
-    model_args.add_argument('--batch_size'
-                            , type=int, default=1
-                            , help='Batch size for training and evaluation')
-    model_args.add_argument('--num_workers'
-                            , type=int, default=0
-                            , help='Number of data loader workers')
-    model_args.add_argument('--num_classes'
-                            , type=int, default=16652
-                            , help='Number of classes (family labels)')
-    model_args.add_argument('--lr'
-                            , type=float, default=1e-2
-                            , help='Learning rate')
-    model_args.add_argument('--momentum'
-                            , type=float, default=0.9
-                            , help='SGD momentum')
-    model_args.add_argument('--weight_decay'
-                            , type=float, default=1e-2
-                            , help='Weight decay for regularization')
-    model_args.add_argument('--gpus'
-                            , type=int, default=0
-                            , help='Number of GPUs to use for training')
-    model_args.add_argument('--epochs'
-                            , type=int, default=25
-                            , help='Number of training epochs')
+    model_args.add_argument('--seq_max_len',
+                            type=int, default=120,
+                            help='Maximum length of sequences')
+    model_args.add_argument('--batch_size',
+                            type=int, default=1,
+                            help='Batch size for training and evaluation')
+    model_args.add_argument('--num_workers',
+                            type=int, default=1,
+                            help='Number of data loader workers')
+    model_args.add_argument('--num_classes',
+                            type=int, default=16652,
+                            help='Number of classes (family labels)')
+    model_args.add_argument('--lr',
+                            type=float, default=1e-2,
+                            help='Learning rate')
+    model_args.add_argument('--momentum',
+                            type=float, default=0.9,
+                            help='SGD momentum')
+    model_args.add_argument('--weight_decay',
+                            type=float, default=1e-2,
+                            help='Weight decay for regularization')
+    model_args.add_argument('--gpus',
+                            type=int, default=1,
+                            help='Number of GPUs to use for training')
+    model_args.add_argument('--epochs',
+                            type=int, default=25,
+                            help='Number of training epochs')
 
-    
     return parser.parse_args()
 
-def check_arguments(options):
+def check_directory(path: Optional[str], description: str) -> None:
+    """Check if the directory exists or create it."""
+    if path:
+        if not os.path.exists(path):
+            os.makedirs(path)
+        elif not os.path.isdir(path):
+            write_logs(f"'{path}' is not a valid {description}. EXIT",
+                       LogStatus.CRITICAL, True)
+
+def check_positive_integer(value: int, name: str) -> None:
+    """Check if the value is a positive integer."""
+    if value <= 0 or not isinstance(value, int):
+        write_logs(f"Invalid {name}: {value}. It should be a positive integer.",
+                   LogStatus.CRITICAL, True)
+
+def check_float_range(value: float, name: str, min_val: float = 0, max_val: float = 1) -> None:
+    """Check if the float value is within the specified range."""
+    if value < min_val or value > max_val:
+        write_logs(f"Invalid {name}: {value}. It should be between {min_val} and {max_val}.",
+                   LogStatus.CRITICAL, True)
+
+def check_arguments(options: argparse.Namespace) -> None:
     """Validate the arguments"""
+    check_directory(options.data_path, 'data directory')
+    check_directory(options.log_path, 'log directory')
+    check_directory(options.output_path, 'output directory')
 
-    if options.data_path and not os.path.isfile(options.data_path):
-        write_logs(f"'{options.data_path}' does not exist. EXIT"
-                   , LogStatus.CRITICAL
-                   , True)
+    # Check if the specified GPUs are available
+    if options.gpus > 0 and not torch.cuda.is_available():
+        available_gpu_count = torch.cuda.device_count()
+        if not available_gpu_count:
+            write_logs("GPU not available, training will be performed on CPU.",
+                    LogStatus.WARNING, True)
+        elif options.gpus != available_gpu_count:
+            write_logs("GPU are not optimized",
+                    LogStatus.WARNING, True)            
 
-    if options.log_path:
-        os.makedirs(options.log_path, exist_ok=True)
+    check_positive_integer(options.batch_size, 'batch size')
+    check_positive_integer(options.num_workers, 'num workers')
+    check_positive_integer(options.num_classes, 'number of classes')
+    check_positive_integer(options.seq_max_len, 'number of classes')
+    check_float_range(options.lr, 'learning rate', 0, 1)
+    check_float_range(options.momentum, 'momentum', 0, 1)
+    check_positive_integer(options.epochs, 'number of epochs')
 
-    # # Check GPU availability
-    # if options.gpus > 0 and not torch.cuda.is_available():
-    #     msg = {
-    #         "msg": "GPU not available, training will be performed on CPU.",
-    #         "status": "warning",
-    #         "echo": True
-    #     }
-    #     logs.append(msg)
+    # Check if log_path and output_path are writable
+    try:
+        for path in [options.log_path, options.output_path]:
+            if path:
+                test_file = os.path.join(path, 'test_write_permission.txt')
+                with open(test_file, 'w') as test_fp:
+                    test_fp.write("Testing write permission.")
+                os.remove(test_file)
+    except IOError:
+        write_logs("No write permission to the specified directory. EXIT",
+                   LogStatus.CRITICAL, True)
--- src/main.py
+++ src/main.py
@@ -11,11 +11,6 @@ def main():
     args = parse_args()
     initialize_logging(args)
     check_arguments(args)
-    # ... (initialisation du modèle, des dataloaders, etc.)
-    # dataloaders = get_dataloaders(args.data_path, args.seq_max_len, args.batch_size, args.num_workers)
-    # model = ProtCNN(args.num_classes)
-
-    # train_protein_classifier(model, dataloaders, args.epochs, args.gpus)
 
 if __name__ == "__main__":
     main()
